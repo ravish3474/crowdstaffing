7cb355bf13dd79fc6b04d21400b45512df8b089a
MIRZA RAVISH <ravish3474@outlook.com>
2020-07-08 13:57:27 +0530

    rav

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, '../frontend', 'build')));
 
