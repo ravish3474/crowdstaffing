5b83e8da9257dbd642f23e131e6e1ccf1cf5eb47
MIRZA RAVISH <ravish3474@outlook.com>
2020-07-08 17:42:51 +0530

    rav

--- backend/server.compiled.js
+++ backend/server.compiled.js
@@ -28,13 +28,13 @@ mongoose.connect(uri, {
 app.use(cors());
 app.use(express.json()); // const jobsRouter = require('./routes/jobs');
 
-var jobseekerRouter = require('../backend/routes/jobseeker');
+var jobseekerRouter = require('/var/app/current/backend/routes/jobseeker');
 
-var categoryRouter = require('../backend/routes/category');
+var categoryRouter = require('/var/app/current/backend/routes/category');
 
-var jobTypeRouter = require('../backend/routes/jobType');
+var jobTypeRouter = require('/var/app/current/backend/routes/jobType');
 
-var skillRouter = require('../backend/routes/skills'); //const resumeRouter = require('./routes/resume');
+var skillRouter = require('/var/app/current/backend/routes/skills'); //const resumeRouter = require('./routes/resume');
 // app.use('/jobs',jobsRouter);
 
 
